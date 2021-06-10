a34b0799605872e82605ba06e1acbbef947b84b3
13588386674 <1592980573@qq.com>
2021-06-10 14:31:53 +0800

    完成了公告的增删查改

--- src/main/java/cn/edu/zucc/controller/NoticeController.java
+++ src/main/java/cn/edu/zucc/controller/NoticeController.java
@@ -1,8 +1,10 @@
 package cn.edu.zucc.controller;
 
 
+import cn.edu.zucc.entity.Notice;
 import cn.edu.zucc.entity.vo.NoticeAssVo;
 import cn.edu.zucc.response.Result;
+import cn.edu.zucc.service.AssociationService;
 import cn.edu.zucc.service.NoticeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -31,6 +33,8 @@ public class NoticeController {
 
     @Resource
     private NoticeService noticeService;
+    @Resource
+    private AssociationService associationService;
 
     /**
      * 查看所有公告
@@ -47,6 +51,73 @@ public class NoticeController {
         else {
             return Result.ok().data("notices", noticeAssVos);
         }
+    }
+
+    /**
+     * 社长发布公告
+     * @param assId
+     * @param noticeName
+     * @param noticeBrief
+     * @param noticePicture
+     * @return
+     */
+    @GetMapping("/addNotice")
+    @ApiOperation(value = "社长发布公告",notes = "社长发布公告")
+    public Result addNotice(@RequestParam("assId") String assId,
+                            @RequestParam("noticeName") String noticeName,
+                            @RequestParam("noticeBrief") String noticeBrief,
+                            @RequestParam("noticePicture") String noticePicture)
+    {
+        Notice notice = new Notice();
+        notice.setAssId(Long.valueOf(assId));
+        notice.setNoticeName(noticeName);
+        notice.setNoticeBrief(noticeBrief);
+        notice.setNoticePicture(noticePicture);
+        noticeService.save(notice);
+        String assName = associationService.getById(assId).getAssName();
+        return Result.ok().data("notice",notice).data("assName",assName);
+    }
+
+    /**
+     * 社长修改公告
+     * @param noticeId
+     * @param noticeName
+     * @param noticeBrief
+     * @param noticePicture
+     * @return
+     */
+    @GetMapping("/modifyNotice")
+    @ApiOperation(value = "社长修改公告",notes = "社长修改公告")
+    public Result modifyNotice(@RequestParam("noticeId") String noticeId,
+                               @RequestParam("noticeName") String noticeName,
+                               @RequestParam("noticeBrief") String noticeBrief,
+                               @RequestParam("noticePicture") String noticePicture){
+        Notice notice = new Notice();
+        notice.setNoticeId(Long.valueOf(noticeId));
+        notice.setNoticeName(noticeName);
+        notice.setNoticeBrief(noticeBrief);
+        notice.setNoticePicture(noticePicture);
+        noticeService.updateById(notice);
+        notice = noticeService.getById(Long.valueOf(noticeId));
+        String assName = associationService.getById(notice.getAssId()).getAssName();
+        return Result.ok().data("notice",notice).data("assName",assName);
+    }
+
+    /**
+     * 社长删除公告
+     * @param noticeId
+     * @return
+     */
+    @GetMapping("/deleteNotice")
+    @ApiOperation(value = "社长删除公告",notes = "社长删除公告")
+    public Result deleteNotice(@RequestParam("noticeId") String noticeId){
+        if(noticeService.removeById(noticeId)) {
+            return Result.ok().data("提示", "删除社团成功");
+        }
+        else {
+            return Result.error().data("提示","不存在该公告");
+        }
+
     }
 }
 
